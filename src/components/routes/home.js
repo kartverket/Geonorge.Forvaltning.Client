a82ffb093be7815b41330b0bd783e073d8f887bb
Dag Olav Dahle <dagolav@arkitektum.no>
2023-09-12 18:21:43 +0200

    Remove unused code

--- src/components/routes/home.js
+++ src/components/routes/home.js
@@ -8,8 +8,6 @@ import { ContentContainer, HeadingText } from "@kartverket/geonorge-web-componen
 
 
 const Home = () => {
-
-  console.log(process.env.SUPABASE_URL);
   
     const handleLogin = async (event) => {
       event.preventDefault()
@@ -24,7 +22,7 @@ const Home = () => {
       if (error) {
         alert(error.error_description || error.message)
       }
-      
+
     };
     
     return (
