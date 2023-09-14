b514e318bc440639713e5aacac119e7464d3abb6
Dag Olav Dahle <dagolav@arkitektum.no>
2023-09-14 11:37:32 +0200

    Login/logout

--- src/components/routes/home.js
+++ src/components/routes/home.js
@@ -1,39 +1,77 @@
 // Dependencies
-import React, { Fragment, useState  } from "react";
+import React, { Fragment, useState, useEffect, useMemo   } from "react";
 import { supabase } from './supabaseClient'
 
 // Geonorge WebComponents
 // eslint-disable-next-line no-unused-vars
 import { ContentContainer, HeadingText } from "@kartverket/geonorge-web-components";
+import Cookies from 'universal-cookie';
 
 
 const Home = () => {
   
-    const handleLogin = async (event) => {
-      event.preventDefault()
-  
-      const { data, error } = await supabase.auth.signInWithOAuth({
-        provider: 'keycloak',
-        options: {
-          scopes: 'openid'
-        },
-      })
-    
-      if (error) {
-        alert(error.error_description || error.message)
-      }
+  const [loggedIn, setLoggedIn] = useState(false);
 
-    };
-    
+  const cookies = useMemo(() => new Cookies(), []);
+
+  cookies.set("loggedIn", cookies.get('loggedIn'));
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: 'keycloak',
+      options: {
+        scopes: 'openid'
+      },
+    })
+
+    console.log(data);
+  
+    if (error) {
+      alert(error.error_description || error.message)
+    }
+    else
+    {
+      cookies.set("loggedIn", true);
+      setLoggedIn(true);
+    }
+
+  };
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+
+    const { error } = await supabase.auth.signOut();
+    if (error) {
+      console.log("Error:" + error);
+    }
+    cookies.set("loggedIn", false);
+    setLoggedIn(false);
+  }
+
+  useEffect(() => {
+
+    setLoggedIn(cookies.get('loggedIn'))
+
+  }, [loggedIn, cookies]);
+
     return (
        
            <Fragment>
                 <heading-text>
                     <h1 underline="true">Forvaltning </h1>
                 </heading-text>
+                {!loggedIn &&
                 <p>
                 <button onClick={handleLogin}>Logg in</button>
                 </p>
+                }
+                {loggedIn &&
+                <p>
+                <button onClick={handleLogout}>Logg ut</button>
+                </p>
+                }
 
             </Fragment>
          
