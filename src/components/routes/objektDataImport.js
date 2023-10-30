a3fc3bce0b26a3e732ae4957f201a66273514702
Dag Olav Dahle <dagolav@arkitektum.no>
2023-10-30 13:39:47 +0100

    More import

--- src/components/routes/objektDataImport.js
+++ src/components/routes/objektDataImport.js
@@ -16,6 +16,29 @@ const ObjektDataImport = () => {
   const [array, setArray] = useState([]);
   const [objekt, setObject] = useState([]);
 
+  const [user, setUser] = useState(undefined || {});
+
+  const [session, setSession] = useState(undefined || {});
+
+  const [showSuccessDialog, setShowSuccessDialog] = useState(false);
+  const [showErrorDialog, setShowErrorDialog] = useState(false);
+  const [errorMessage, setErrorMessage] = useState();
+
+
+  const setShowSuccessDialogBox = () => {
+    setShowSuccessDialog(false);
+    setTimeout(() => {
+        setShowSuccessDialog(true);
+    });
+  };
+
+  const showDialogErrorBox = () => {
+      setShowErrorDialog(false);
+      setTimeout(() => {
+          setShowErrorDialog(true);
+      });
+  };
+
   const fileReader = new FileReader();
 
   const handleOnChange = (e) => {
@@ -23,8 +46,9 @@ const ObjektDataImport = () => {
   };
 
   const csvFileToArray = string => {
-    const csvHeader = string.slice(0, string.indexOf("\n")).split(";");
-    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
+    const csvHeader = string.slice(0, string.indexOf("\r\n")).split(";");
+    var csvRows = string.slice(string.indexOf("\n") + 1).split("\r\n");
+    csvRows = csvRows.slice(0, csvRows.length - 1);
 
     const array = csvRows.map(i => {
       const values = i.split(";");
@@ -55,7 +79,6 @@ const ObjektDataImport = () => {
   const headerKeys = Object.keys(Object.assign({}, ...array));
 
   const fetchObject = async (event) => {
-    var metaAndData = null;
     await supabase
     .from('ForvaltningsObjektMetadata')
     .select(`
@@ -68,11 +91,118 @@ const ObjektDataImport = () => {
 
     console.log(objekt);
 
+
   }
+  const fetchUser = async () => {
+
+    await supabase.from('users')
+    .select('organization,role')
+    .then((res) => { setUser(res); console.log(res);})
+    .catch((err => console.log(err)))
+  
+    supabase.auth.getSession().then((res) => { setSession(res) })
+    .catch((err => console.log(err)))
+  
+  }
+
+  const handleImport = async (event) => {
+    event.preventDefault();
+
+    var propName;
+    var value;
+    var dataType;
+    var columnName = '';
+    var tableName = objekt.data[0].TableName;
+
+    console.log(tableName);
+
+    array.map( async (item) => {
+      var su = '{';
+
+        for(var i = 0; i < objekt.data[0].ForvaltningsObjektPropertiesMetadata.length; i++)
+        {
+          var o2 = objekt.data[0].ForvaltningsObjektPropertiesMetadata[i];
+          if(o2.name !== "id")
+          {
+            propName = o2.Name;
+            dataType = o2.DataType;
+            value = item[propName]?.toString()  //event.target[propName].value;
+            columnName = o2.ColumnName;
+            if(columnName == null)
+              columnName = propName;
+
+            console.log(propName + ":" + value);
+
+            if(dataType == "bool" || dataType == "numeric")
+            {
+              su = su + ' "'+ columnName +'" : '+ value +' ';
+            }
+            else{
+              su = su + ' "'+ columnName +'" : "'+ value +'" ';
+            }
+
+            if( i < objekt.data[0].ForvaltningsObjektPropertiesMetadata.length -1)
+            {
+              su = su + ",";
+            }
+        }
+        }
+
+        console.log(session);
+
+        //su = su  + ' , "geometry" : '+ JSON.stringify(event.target['geometry'].value) +' ';
+
+        su = su  + ' , "owner_org" : "'+ user.data[0].organization +'" ';
+
+        su = su  + ' , "editor" : "'+ session.data.session.user.email +'" ';
+
+        su = su  + ' , "updatedate" : "'+ ((new Date()).toISOString()).toLocaleString('no-NO') +'" ';
+        
+        su = su + "}";
+
+        console.log(su);
+
+        var insert = JSON.parse(su);
+        console.log(insert);
+
+        const { error } = await supabase
+        .from(tableName)
+        .insert(insert)
+
+        console.log(error);
+
+        if(error == null)
+        {
+          setShowSuccessDialogBox();
+        }
+        else
+        {
+          showDialogErrorBox();
+
+          if (error.response?.data) {
+              const messages = Object.values(error.response.data).map((value) => value.join(", "));
+              setErrorMessage(messages.join("\r\n"));
+          }
+          else if (error?.message) 
+          { 
+            setErrorMessage(error.message);
+          }
+          else {
+              setErrorMessage(error);
+          }
+        }
+        
+        console.log(error)
+      
+      });
+  }
+
+
 
   useEffect(() => {
 
     fetchObject();
+    fetchUser();
 
   }, []);
 
@@ -97,7 +227,14 @@ const ObjektDataImport = () => {
         >
           Importer CSV
         </button>
-        <input style={{float: "right"}} type="submit" value="Lagre"/>
+        <input style={{float: "right"}} type="submit" value="Lagre" onClick={handleImport}/>
+        <gn-dialog show={showSuccessDialog} width="" overflow="">
+                <body-text>Dataene ble lagt til</body-text>
+            </gn-dialog>
+
+            <gn-dialog show={showErrorDialog} width="" overflow="">
+                <body-text>{errorMessage}</body-text>
+        </gn-dialog>
       </form>
 
       <br />
@@ -110,7 +247,6 @@ const ObjektDataImport = () => {
               <th key={d.Name}>{d.Name}</th>
           )
       }
-      <th>geometry</th>
       </tr>
       </thead>
       <tbody>
