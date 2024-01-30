91e5d410469ad01f0f68657d750aaa6bec39dfa8
Dag Olav Dahle <dagolav@arkitektum.no>
2024-01-30 12:50:41 +0100

    Display login error description

--- src/features/Login/index.jsx
+++ src/features/Login/index.jsx
@@ -3,7 +3,10 @@ import styles from './Login.module.scss';
 
 export default function Login() {
    const { signIn } = useAuth()
-   
+
+   const queryParameters = new URLSearchParams(window.location.search)
+   const error_description = queryParameters.get("error_description")
+
    return (
       <div className={styles.container}>
          <div className={styles.login}>
@@ -15,6 +18,9 @@ export default function Login() {
                <gn-button>
                   <button onClick={signIn}>Logg inn</button>
                </gn-button>
+            </div>
+            <div className={styles.error}>
+               {error_description}
             </div>
          </div>
       </div>
