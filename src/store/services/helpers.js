b105babb5791045ee337bdfc615d4e64d0f82717
Dag Olav Dahle <dagolav@arkitektum.no>
2024-10-16 11:24:00 +0200

    Hent bidragsytere fra metadata for egenskaper

--- src/store/services/helpers.js
+++ src/store/services/helpers.js
@@ -1,17 +1,39 @@
 import dayjs from 'dayjs';
+import { forEach } from 'lodash';
 import store from 'store';
 
 function addCommonProperties(data, ownerOrg, definition) {
    const user = store.getState().app.user;
-
    if (user !== null) {
       data['owner_org'] = ownerOrg;
-      data['contributor_org'] = definition.Contributors;
+      data['contributor_org'] = getContributors(definition);
       data['viewer_org'] = definition.Viewers;      ;
       data['editor'] = user.email;
    }
 
    data['updatedate'] = dayjs().format();
+}
+
+function getContributors(definition) {
+
+   let contributors = definition.Contributors;
+
+   if (contributors === null) {
+      let contributorByProperty = [];
+      definition.ForvaltningsObjektPropertiesMetadata.forEach((property) => {
+         property.AccessByProperties.forEach((access) => {
+            var contributors = access.Contributors;
+            contributorByProperty = contributorByProperty.concat(contributors);          
+         });
+      });
+
+      if(contributorByProperty.length > 0) {
+         var contributorByPropertyDistinct = Array.from(new Set(contributorByProperty));
+         contributors = contributorByPropertyDistinct;
+      }
+   } 
+
+   return contributors;
 }
 
 export default addCommonProperties;
