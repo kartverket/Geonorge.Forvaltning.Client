d2c02bf2dcb9b0f7463f8ae356d80a458b55a62c
Dag Olav Dahle <dagolav@arkitektum.no>
2025-01-13 14:24:15 +0100

    Update dataset object

--- src/store/services/supabase/mutations/dataObject.js
+++ src/store/services/supabase/mutations/dataObject.js
@@ -1,4 +1,6 @@
 import supabase from 'store/services/supabase/client';
+import { getAccessToken } from 'store/services/supabase/client';
+import environment from 'config/environment';
 
 export async function addDatasetObject(payload, table) {
 
@@ -22,10 +24,36 @@ export async function addDatasetObjects(payload, table) {
 
 export async function updateDatasetObject(id, payload, table) {
 
-      const { error } = await supabase
+      /*const { error } = await supabase
       .from(table)
       .update(payload)
-      .eq('id', id)
+      .eq('id', id)*/
+
+      let error = null;
+      try {
+         const accessToken = await getAccessToken();
+         const bearer = `Bearer ${accessToken}`;
+
+         var idDataset = table.replace("t_", "");
+
+         const response = await fetch(environment.API_BASE_URL + '/Object/'+ idDataset, { 
+            method: 'PUT',
+            headers: new Headers({
+               'Content-type': 'application/json',
+               'Authorization': bearer,
+               'Apikey': environment.SUPABASE_ANON_KEY,
+            }),
+            body: JSON.stringify(payload)
+         });
+
+         if (!response.ok) {
+           throw new Error('Network response was not ok.');
+         }
+      }
+      catch (error) {
+         console.error('There has been a problem with your fetch operation:', error);
+      }
+
 
    return { data: null, error };
 }
