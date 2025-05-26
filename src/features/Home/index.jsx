bbe8f98884d13ecbf456f19edc0a699d2c19b432
Sondre Bitubekk <sondre@arkitektum.no>
2025-05-26 18:53:28 +0200

    GEON-1592: Removed header and datasets table from Home

--- src/features/Home/index.jsx
+++ src/features/Home/index.jsx
@@ -1,31 +1,10 @@
 import { useSelector } from 'react-redux';
-import Datasets from './Datasets';
 import RequestAuthorization from './RequestAuthorization';
 
 export default function Home({ datasets }) {
     const user = useSelector(state => state.app.user);
 
-    function renderContents() {
-        if (user === null) {
-            return null;
-        }
-
-        if (user.organization !== null) {
-            return <Datasets datasets={datasets} />
-        }
-
-        return <RequestAuthorization />
-    }
-
     return (
-        <>
-            <heading-text>
-                <h1 underline="true">Mine datasett</h1>
-            </heading-text>
-
-            <div className="container">
-                {renderContents()}
-            </div>
-        </>
+        user !== null && user.organization === null ? <RequestAuthorization /> : <div>Placeholder for map. Number of datasets: {datasets.length}</div>
     );
 }
