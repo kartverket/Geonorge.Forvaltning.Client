a2ef1927858fab4948188f9039ee311e7e877b85
Sondre Bitubekk <sondre@arkitektum.no>
2025-08-27 10:28:21 +0200

    GEON-1592: Fixed cache handling for object changes

--- src/store/services/cacheUpdater.js
+++ src/store/services/cacheUpdater.js
@@ -24,51 +24,40 @@ const cacheUpdater = {
    },
    updateDatasetObject: (dispatch, args) => {
       dispatch(
-         api.util.updateQueryData(
-            "getDataset",
-            args.tableId.toString(),
-            (cachedData) => {
-               const object = cachedData.objects.find(
-                  (object) => object.id === args.payload.id
-               );
-               const objectKeys = Object.keys(object);
+         api.util.updateQueryData("getDataset", args.tableId, (cachedData) => {
+            const object = cachedData.objects.find(
+               (object) => object.id === args.payload.id
+            );
 
-               Object.entries(args.payload)
-                  .filter((entry) => objectKeys.includes(entry[0]))
-                  .forEach((entry) => {
-                     if (entry[0] === "geometry") {
-                        object[entry[0]] = JSON.parse(entry[1]);
-                     } else {
-                        object[entry[0]] = entry[1];
-                     }
-                  });
-            }
-         )
+            const objectKeys = Object.keys(object);
+
+            Object.entries(args.payload)
+               .filter((entry) => objectKeys.includes(entry[0]))
+               .forEach((entry) => {
+                  if (entry[0] === "geometry") {
+                     object[entry[0]] = JSON.parse(entry[1]);
+                  } else {
+                     object[entry[0]] = entry[1];
+                  }
+               });
+         })
       );
    },
    deleteDatasetObjects: (dispatch, args) => {
       dispatch(
-         api.util.updateQueryData(
-            "getDataset",
-            args.tableId.toString(),
-            (cachedData) => {
-               const objects = cachedData.objects.filter(
-                  (object) => !args.ids.includes(object.id)
-               );
-               cachedData.objects = objects;
-            }
-         )
+         api.util.updateQueryData("getDataset", args.tableId, (cachedData) => {
+            const objects = cachedData.objects.filter(
+               (object) => !args.ids.includes(object.id)
+            );
+            cachedData.objects = objects;
+         })
       );
    },
    deleteAllDatasetObjects: (dispatch, args) => {
       dispatch(
-         api.util.updateQueryData(
-            "getDataset",
-            args.tableId.toString(),
-            (cachedData) => {
-               cachedData.objects = [];
-            }
-         )
+         api.util.updateQueryData("getDataset", args.tableId, (cachedData) => {
+            cachedData.objects = [];
+         })
       );
    },
 };
